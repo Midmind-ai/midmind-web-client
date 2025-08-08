ebab779483c0bf88e19fbbe8772b96d4c7a062c1
Max Haponenko <mhap@cbpm-service.com>
2025-08-08 19:24:07 +0300

    data-component attributes

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,46 @@
 import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
+
+// import react from '@vitejs/plugin-react-swc'; << replaced with old plugin-react to support babel custom visitor function
 
 import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr({ include: '**/*.svg' })],
+  plugins: [
+    svgr({ include: '**/*.svg' }),
+    react({
+      babel: {
+        plugins: [
+          // Add data-component attribute to html to help developers navigate through Tailwind classNames
+          function emojiComponentAttribute() {
+            return {
+              visitor: {
+                JSXOpeningElement(path, state) {
+                  // Extract component name from file path
+                  const file = state.file.opts.filename;
+                  const match = file.match(/([^/\\]+)\.(jsx?|tsx?)$/);
+                  const componentName = match?.[1] ?? 'Unknown';
+
+                  path.node.attributes.unshift({
+                    type: 'JSXAttribute',
+                    name: { type: 'JSXIdentifier', name: 'data-component' },
+                    value: {
+                      type: 'StringLiteral',
+                      value: `[!] ${componentName}`,
+                    },
+                  });
+                },
+              },
+            };
+          },
+        ],
+      },
+    }),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
